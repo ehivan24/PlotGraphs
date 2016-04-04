766a8273222003c006b1976958a604e4032a1091
edwing santos <ehivan24@yahoo.com>
2016-04-04 14:40:10 -0400

    android plot

---
+++ app/src/main/java/com/ada/edwingsantos/plotgraphs/MainActivity.java
@@ -0,0 +1,70 @@
+package com.ada.edwingsantos.plotgraphs;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.androidplot.xy.LineAndPointFormatter;
+import com.androidplot.xy.PointLabelFormatter;
+import com.androidplot.xy.SimpleXYSeries;
+import com.androidplot.xy.XYPlot;
+import com.androidplot.xy.XYSeries;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+    private XYPlot mySimpleXYPlot;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        // initialize our XYPlot reference:
+        mySimpleXYPlot = (XYPlot) findViewById(R.id.mySimpleXYPlot);
+
+        // Create a couple arrays of y-values to plot:
+        Number[] series1Numbers = {1, 8, 5, 2, 7, 4};
+        Number[] series2Numbers = {4, 6, 3, 8, 2, 10};
+
+
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        arrayList.add(1);
+        arrayList.add(2);
+        arrayList.add(5);
+        arrayList.add(9);
+        arrayList.add(11);
+
+        // Turn the above arrays into XYSeries':
+        XYSeries series1 = new SimpleXYSeries(
+                Arrays.asList(series1Numbers),          // SimpleXYSeries takes a List so turn our array into a List
+                SimpleXYSeries.ArrayFormat.Y_VALS_ONLY, // Y_VALS_ONLY means use the element index as the x value
+                "Series1");                             // Set the display title of the series
+
+        // same as above
+        XYSeries series2 = new SimpleXYSeries(Arrays.asList(series2Numbers), SimpleXYSeries.ArrayFormat.Y_VALS_ONLY, "Series2");
+
+        // Create a formatter to use for drawing a series using LineAndPointRenderer:
+        LineAndPointFormatter series1Format = new LineAndPointFormatter(
+                Color.rgb(0, 200, 0),                   // line color
+                Color.rgb(0, 100, 0),                   // point color
+                null,                                   // fill color (none)
+                new PointLabelFormatter(Color.WHITE));                           // text color
+
+        // add a new series' to the xyplot:
+        mySimpleXYPlot.addSeries(series1, series1Format);
+
+        // same as above:
+        mySimpleXYPlot.addSeries(series2,
+                new LineAndPointFormatter(
+                        Color.rgb(0, 0, 200),
+                        Color.rgb(0, 0, 100),
+                        null,
+                        new PointLabelFormatter(Color.WHITE)));
+
+        // reduce the number of range labels
+        mySimpleXYPlot.setTicksPerRangeLabel(3);
+    }
+}
